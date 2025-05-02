f0ddcd0bb674831d2d86c6a94363fedddbb579eb
Chery-cake <theodoros16dtbm@gmail.com>
2025-05-02 18:32:39 -0300

    Updated for Godot 4.4

---
+++ code/include/register_types.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <godot_cpp/core/class_db.hpp>
+
+void initialize_library_modules(godot::ModuleInitializationLevel p_level);
+void unitialize_library_modules(godot::ModuleInitializationLevel p_level);
